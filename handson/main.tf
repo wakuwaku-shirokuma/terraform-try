238df3f3361110ba9dfe1fc68115569fb682357c
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-12 00:59:18 +0900

    VPC情報の追加と構築

--- handson/main.tf
+++ handson/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "ap-northeast-1"
 }
+
+# VPC
+# https://www.terraform.io/docs/providers/aws/r/vpc.html
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "handson"
+  }
