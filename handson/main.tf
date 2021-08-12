abefab854ff29138aeab03adf36d140e141662db
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-12 15:09:32 +0900

    Elastic IPの設定とそこに紐づくNAT Gatewayの設置「1セット分」

--- handson/main.tf
+++ handson/main.tf
@@ -95,3 +95,24 @@ resource "aws_internet_gateway" "main" {
     Name = "handson"
   }
 }
+
+# Elastic IP
+# https://www.terraform.io/docs/providers/aws/r/eip.html
+resource "aws_eip" "nat_1a" {
+  vpc = true
+
+  tags = {
+    Name = "handson-natgw-1a"
+  }
+}
+
+# NAT Gateway
+# https://www.terraform.io/docs/providers/aws/r/nat_gateway.html
+resource "aws_nat_gateway" "nat_1a" {
+  subnet_id     = "${aws_subnet.public_1a.id}" # NAT Gatewayを配置するSubnetを指定
+  allocation_id = "${aws_eip.nat_1a.id}"       # 紐付けるElastic IP
+
+  tags = {
+    Name = "handson-1a"
+  }
+}
