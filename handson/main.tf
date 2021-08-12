f802ade85dd39a6f4e87307727341ba3f4d48f41
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-12 15:50:32 +0900

    NAT GatewayとPrivate SubnetをRoute Tableで紐付ける

--- handson/main.tf
+++ handson/main.tf
@@ -185,3 +185,67 @@ resource "aws_route_table_association" "public_1d" {
   subnet_id      = "${aws_subnet.public_1d.id}"
   route_table_id = "${aws_route_table.public.id}"
 }
+
+# Route Table (Private)
+# https://www.terraform.io/docs/providers/aws/r/route_table.html
+resource "aws_route_table" "private_1a" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-private-1a"
+  }
+}
+
+resource "aws_route_table" "private_1c" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-private-1c"
+  }
+}
+
+resource "aws_route_table" "private_1d" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson-private-1d"
+  }
+}
+
+# Route (Private)
+# https://www.terraform.io/docs/providers/aws/r/route.html
+resource "aws_route" "private_1a" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.private_1a.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat_1a.id}"
+}
+
+resource "aws_route" "private_1c" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.private_1c.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat_1c.id}"
+}
+
+resource "aws_route" "private_1d" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = "${aws_route_table.private_1d.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat_1d.id}"
+}
+
+# Association (Private)
+# https://www.terraform.io/docs/providers/aws/r/route_table_association.html
+resource "aws_route_table_association" "private_1a" {
+  subnet_id      = "${aws_subnet.private_1a.id}"
+  route_table_id = "${aws_route_table.private_1a.id}"
+}
+
+resource "aws_route_table_association" "private_1c" {
+  subnet_id      = "${aws_subnet.private_1c.id}"
+  route_table_id = "${aws_route_table.private_1c.id}"
+}
+
+resource "aws_route_table_association" "private_1d" {
+  subnet_id      = "${aws_subnet.private_1d.id}"
+  route_table_id = "${aws_route_table.private_1d.id}"
+}
+
