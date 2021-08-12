e1abad01fd021cb9ff938717e7cb3e8352cf2111
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-12 14:59:44 +0900

    インターネット gatewayの設置

--- handson/main.tf
+++ handson/main.tf
@@ -85,3 +85,13 @@ resource "aws_subnet" "private_1d" {
     Name = "handson-private-1d"
   }
 }
+
+# Internet Gateway
+# https://www.terraform.io/docs/providers/aws/r/internet_gateway.html
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "handson"
+  }
+}
