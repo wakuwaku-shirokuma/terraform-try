2dfc161364c2505e2b632332959408ce02d82a6f
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-12 15:29:47 +0900

    Elastic IPの設定とそこに紐づくNAT Gatewayの設定「3セット分」

--- handson/main.tf
+++ handson/main.tf
@@ -116,3 +116,37 @@ resource "aws_nat_gateway" "nat_1a" {
     Name = "handson-1a"
   }
 }
+
+resource "aws_eip" "nat_1c" {
+  vpc = true
+
+  tags = {
+    Name = "handson-natgw-1c"
+  }
+}
+
+resource "aws_nat_gateway" "nat_1c" {
+  subnet_id     = "${aws_subnet.public_1c.id}"
+  allocation_id = "${aws_eip.nat_1c.id}"
+
+  tags = {
+    Name = "handson-1c"
+  }
+}
+
+resource "aws_eip" "nat_1d" {
+  vpc = true
+
+  tags = {
+    Name = "handson-natgw-1d"
+  }
+}
+
+resource "aws_nat_gateway" "nat_1d" {
+  subnet_id     = "${aws_subnet.public_1d.id}"
+  allocation_id = "${aws_eip.nat_1d.id}"
+
+  tags = {
+    Name = "handson-1d"
+  }
+}
