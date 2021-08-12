0825eb91711e9b4b3adeeb13ef784fc9ff0d91f6
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-12 14:33:40 +0900

    とりあえず一つサブネットを作成する

--- handson/main.tf
+++ handson/main.tf
@@ -10,3 +10,20 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "handson"
   }
+}
+
+# Subnet
+# https://www.terraform.io/docs/providers/aws/r/subnet.html
+resource "aws_subnet" "public_1a" {
+  # 先程作成したVPCを参照し、そのVPC内にSubnetを立てる
+  vpc_id = "${aws_vpc.main.id}"
+
+  # Subnetを作成するAZ
+  availability_zone = "ap-northeast-1a"
+
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Name = "handson-public-1a"
+  }
+}
