d4e26d5e6a17b22ff3251c798956e64ce63591d3
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-17 17:24:34 +0900

    【ECS】サービスの作成（DNSのAレコードでwebにアクセスし、nginxの起動を確認するところまで）

--- handson/main.tf
+++ handson/main.tf
@@ -397,3 +397,81 @@ resource "aws_lb_listener_rule" "main" {
     }
   }
 }
+
+# SecurityGroup
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group" "ecs" {
+  name        = "handson-ecs"
+  description = "handson ecs"
+
+  # セキュリティグループを配置するVPC
+  vpc_id      = "${aws_vpc.main.id}"
+
+  # セキュリティグループ内のリソースからインターネットへのアクセス許可設定
+  # 今回の場合DockerHubへのPullに使用する。
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "handson-ecs"
+  }
+}
+
+# SecurityGroup Rule
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group_rule" "ecs" {
+  security_group_id = "${aws_security_group.ecs.id}"
+
+  # インターネットからセキュリティグループ内のリソースへのアクセス許可設定
+  type = "ingress"
+
+  # TCPでの80ポートへのアクセスを許可する
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  # 同一VPC内からのアクセスのみ許可
+  cidr_blocks = ["10.0.0.0/16"]
+}
+
+# ECS Service
+# https://www.terraform.io/docs/providers/aws/r/ecs_service.html
+resource "aws_ecs_service" "main" {
+  name = "handson"
+
+  # 依存関係の記述。
+  # "aws_lb_listener_rule.main" リソースの作成が完了するのを待ってから当該リソースの作成を開始する。
+  # "depends_on" は "aws_ecs_service" リソース専用のプロパティではなく、Terraformのシンタックスのため他の"resource"でも使用可能
+  depends_on = ["aws_lb_listener_rule.main"]
+
+  # 当該ECSサービスを配置するECSクラスターの指定
+  cluster = "${aws_ecs_cluster.main.name}"
+
+  # データプレーンとしてFargateを使用する
+  launch_type = "FARGATE"
+
+  # ECSタスクの起動数を定義
+  desired_count = "1"
+
+  # 起動するECSタスクのタスク定義
+  task_definition = "${aws_ecs_task_definition.main.arn}"
+
+  # ECSタスクへ設定するネットワークの設定
+  network_configuration {
+    # タスクの起動を許可するサブネット
+    subnets         = ["${aws_subnet.private_1a.id}", "${aws_subnet.private_1c.id}", "${aws_subnet.private_1d.id}"]
+    # タスクに紐付けるセキュリティグループ
+    security_groups = ["${aws_security_group.ecs.id}"]
+  }
+
+  # ECSタスクの起動後に紐付けるELBターゲットグループ
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.main.arn}"
+    container_name   = "nginx"
+    container_port   = "80"
+  }
+}
