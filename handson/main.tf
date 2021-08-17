a976bce6f806e40d55f948b2d3386f3c7baea6ea
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-17 15:47:41 +0900

    【ECS】タスク定義（1つ分の作成）

--- handson/main.tf
+++ handson/main.tf
@@ -316,3 +316,38 @@ resource "aws_lb_listener" "main" {
     }
   }
 }
+
+# Task Definition
+# https://www.terraform.io/docs/providers/aws/r/ecs_task_definition.html
+resource "aws_ecs_task_definition" "main" {
+  family = "handson"
+
+  # データプレーンの選択
+  requires_compatibilities = ["FARGATE"]
+
+  # ECSタスクが使用可能なリソースの上限
+  # タスク内のコンテナはこの上限内に使用するリソースを収める必要があり、メモリが上限に達した場合OOM Killer にタスクがキルされる
+  cpu    = "256"
+  memory = "512"
+
+  # ECSタスクのネットワークドライバ
+  # Fargateを使用する場合は"awsvpc"決め打ち
+  network_mode = "awsvpc"
+
+  # 起動するコンテナの定義
+  # 「nginxを起動し、80ポートを開放する」設定を記述。
+  container_definitions = <<EOL
+[
+  {
+    "name": "nginx",
+    "image": "nginx:1.20",
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+EOL
+}
