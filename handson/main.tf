39df1cbf18dfdbc031dd08a5e10e5d9af707db01
Iwata hayato <wakuwaku.shirokuma.works@gmail.com>
2021-08-17 15:29:28 +0900

    ALB（Application Load Balancer）の設定とそれに紐づくリスナーの作成

--- handson/main.tf
+++ handson/main.tf
@@ -249,3 +249,70 @@ resource "aws_route_table_association" "private_1d" {
   route_table_id = "${aws_route_table.private_1d.id}"
 }
 
+# SecurityGroup
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group" "alb" {
+  name        = "handson-alb"
+  description = "handson alb"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  # セキュリティグループ内のリソースからインターネットへのアクセスを許可する
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "handson-alb"
+  }
+}
+
+# SecurityGroup Rule
+# https://www.terraform.io/docs/providers/aws/r/security_group.html
+resource "aws_security_group_rule" "alb_http" {
+  security_group_id = "${aws_security_group.alb.id}"
+
+  # セキュリティグループ内のリソースへインターネットからのアクセスを許可する
+  type = "ingress"
+
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# ALB
+# https://www.terraform.io/docs/providers/aws/d/lb.html
+resource "aws_lb" "main" {
+  load_balancer_type = "application"
+  name               = "handson"
+
+  security_groups = ["${aws_security_group.alb.id}"]
+  subnets         = ["${aws_subnet.public_1a.id}", "${aws_subnet.public_1c.id}", "${aws_subnet.public_1d.id}"]
+}
+
+# Listener
+# https://www.terraform.io/docs/providers/aws/r/lb_listener.html
+resource "aws_lb_listener" "main" {
+  # HTTPでのアクセスを受け付ける
+  port              = "80"
+  protocol          = "HTTP"
+
+  # ALBのarnを指定します。
+  #XXX: arnはAmazon Resource Names の略で、その名の通りリソースを特定するための一意な名前(id)です。
+  load_balancer_arn = "${aws_lb.main.arn}"
+
+  # "ok" という固定レスポンスを設定する
+  default_action {
+    type             = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "200"
+      message_body = "ok"
+    }
+  }
+}
